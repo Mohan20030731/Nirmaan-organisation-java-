28044189cab68d40db276aace81439fefa1da133
Mohan20030731 <167453459+Mohan20030731@users.noreply.github.com>
2024-11-28 13:30:30 +0530

    Add files via upload
    DAY7 PART 2

--- StudentDetails.java
+++ StudentDetails.java
@@ -12,7 +12,7 @@ public class StudentDetails {
 		std.stName = "Mohan kumar";
 		std.age = 21;
 		std.gender = "Male";
-		std.qlf = "B.sc computer science";
+		std.qlf = "B.Sc Computer Science";
 		System.out.println(std.stName);
 		System.out.println(std.age);
 		System.out.println(std.gender);
@@ -23,7 +23,7 @@ public class StudentDetails {
 		std1.stName = "Loganathan";
 		std1.age = 21;
 		std1.gender = "Male";
-		std1.qlf = "B.sc computer science";
+		std1.qlf = "B.Sc Computer Science";
 		System.out.println(std1.stName);
 		System.out.println(std1.age);
 		System.out.println(std1.gender);
@@ -34,11 +34,22 @@ public class StudentDetails {
 		std2.stName = "Ashwin";
 		std2.age = 20;
 		std2.gender = "Male";
-		std2.qlf = "B.sc computer science";
+		std2.qlf = "B.sc Computer Science";
 		System.out.println(std2.stName);
 		System.out.println(std2.age);
 		System.out.println(std2.gender);
 		System.out.println(std2.qlf);
+		System.out.println(" ");
+		
+		StudentDetails std3 = new StudentDetails();
+		std3.stName = "Nivashini";
+		std3.age = 21;
+		std3.gender = "female";
+		std3.qlf = "B.com ISM";
+		System.out.println(std3.stName);
+		System.out.println(std3.age);
+		System.out.println(std3.gender);
+		System.out.println(std3.qlf);
 		
 		}
 }
