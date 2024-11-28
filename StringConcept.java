a96161806a09df66f238c5bef51956508312a6e0
Mohan20030731 <167453459+Mohan20030731@users.noreply.github.com>
2024-11-28 12:32:41 +0530

    Add files via upload
    day7

---
+++ StringConcept.java
@@ -0,0 +1,12 @@
+package day7;
+import java.util.Scanner;
+public class StringConcept {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your Name :");
+		String name = "Mohan Kumar";
+		System.out.println(name.substring(0,5));
+		sc.close();
+	}
+
+}
