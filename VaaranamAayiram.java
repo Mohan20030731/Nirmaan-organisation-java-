41ebbc7faa2ce95d92e689c495416acd9c18182f
Mohan20030731 <167453459+Mohan20030731@users.noreply.github.com>
2024-12-03 13:22:23 +0530

    Add files via upload
    day8 part1

---
+++ VaaranamAayiram.java
@@ -0,0 +1,17 @@
+package day8;
+
+public class VaaranamAayiram {
+   	public static void main(String[] args) {
+   		boolean isAlive=false;
+   		if(isAlive) 
+   		{
+   			System.out.println("Megana married Suriya!");
+   		} 
+   			else
+        {
+   			System.out.println("Suriya get Arrange Marriage :)");	
+		
+}
+		
+}
+}
